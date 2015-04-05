02c95e00c70747cc8566a1be3909f9e9df0b60e7
Moïse Valvassori <moise.valvassori@gmail.com>
2015-04-05 08:28:23 +0200

    formatting

--- server/startup.js
+++ server/startup.js
@@ -1,51 +1,52 @@
 var errorCount=0;
 
 docker_init = function(hostId) {
-    check(hostId, checkHostId);
-    var host = modules.collections.Hosts.findOne({_id:hostId});
-    if (host === null || host === undefined)
-        return;
-    docker[host._id] = new Docker(host.connection);
-    try{
-	docker[host._id].version(Meteor.bindEnvironment(function (err, version) {
-            if (err)
-		return;
-            var u = modules.collections.Hosts.update({_id:host._id}, {$set: {version:version}}, {validate:false, filter: false});
-	}));
-	
-	docker[host._id].getEvents({}, Meteor.bindEnvironment(
-            function(err,stream){
-		if (err){
-                    console.log(host, err);
-                    modules.collections.Hosts.upsert({_id:host._id}, {$set: {status:false, lastError:err}}, {validate:false, filter: false});
-                    console.log('err',err);
-                    Meteor.setTimeout(function() {
-			docker_init(host._id);
-                    }, Math.round(10000/(1+ Math.exp(-0.15*(errorCount-30)))) );
-                    errorCount++;
-		}
-		else {
-                    modules.collections.Hosts.upsert({_id:host._id}, {$set: {status:true}}, {validate:false, filter: false});
-                    errorCount=0;
-		    docker[host._id]._eventStream = stream;
-                    stream.on('data', Meteor.bindEnvironment(
-			function(chunk) {
-                            var event = JSON.parse(chunk.toString());
-                            eventHandle(host._id,event);
-                            modules.collections.Hosts.upsert({_id:host._id}, {$set: {status:true}}, {validate:false, filter: false});
-			})).on('end', Meteor.bindEnvironment(
-			    function(error) {
-				modules.collections.Hosts.upsert({_id:host._id}, {$set: {status:false, lastError:err}}, {validate:false, filter: false});
-				docker_init(host._id);
-			    })).on('error', Meteor.bindEnvironment(
-				function(error) {
-				    modules.collections.Hosts.upsert({_id:host._id}, {$set: {status:false, lastError:err}}, {validate:false, filter: false});
-				    docker_init(host._id);
-				}));
-		}}));
-    } catch(exp) {
-        console.log(exp);
-    }
+  check(hostId, checkHostId);
+  var host = modules.collections.Hosts.findOne({_id:hostId});
+  if (host === null || host === undefined)
+    return;
+  docker[host._id] = new Docker(host.connection);
+  try{
+    docker[host._id].version(
+      Meteor.bindEnvironment(function (err, version) {
+        if (err)
+	  return;
+        var u = modules.collections.Hosts.update({_id:host._id}, {$set: {version:version}}, {validate:false, filter: false});
+      }));
+    
+    docker[host._id].getEvents({}, Meteor.bindEnvironment(
+      function(err,stream){
+	if (err){
+	  if (! host.disabled)
+	    console.log(host, err);
+          modules.collections.Hosts.upsert({_id:host._id}, {$set: {status:false, lastError:err}}, {validate:false, filter: false});
+          Meteor.setTimeout(function() {
+	    docker_init(host._id);
+          }, Math.round(10000/(1+ Math.exp(-0.15*(errorCount-30)))) );
+          errorCount++;
+	}
+	else {
+          modules.collections.Hosts.upsert({_id:host._id}, {$set: {status:true}}, {validate:false, filter: false});
+          errorCount=0;
+	  docker[host._id]._eventStream = stream;
+          stream.on('data', Meteor.bindEnvironment(
+	    function(chunk) {
+              var event = JSON.parse(chunk.toString());
+              eventHandle(host._id,event);
+              modules.collections.Hosts.upsert({_id:host._id}, {$set: {status:true}}, {validate:false, filter: false});
+	    })).on('end', Meteor.bindEnvironment(
+	      function(error) {
+		modules.collections.Hosts.upsert({_id:host._id}, {$set: {status:false, lastError:err}}, {validate:false, filter: false});
+		docker_init(host._id);
+	      })).on('error', Meteor.bindEnvironment(
+		function(error) {
+		  modules.collections.Hosts.upsert({_id:host._id}, {$set: {status:false, lastError:err}}, {validate:false, filter: false});
+		  docker_init(host._id);
+		}));
+	}}));
+  } catch(exp) {
+    console.log(exp);
+  }
 };
 
 
