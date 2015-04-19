3b0bd901284f2c0cbffa7e6cf552d4598cf6526c
Moïse Valvassori <moise.valvassori@gmail.com>
2015-04-19 10:32:14 +0200

    Ensure api for client

--- client/00helpers.js
+++ client/00helpers.js
@@ -1,12 +1,13 @@
 
-
-Template.registerHelper('ensureApi', function(hostId, api) {
+ensureApi = function(hostId, api) {
     var host = Hosts.findOne({_id:hostId});
     if (host){
 	return host.version.ApiVersion >= api;
     }
     return false;
-});
+};
+
+Template.registerHelper('ensureApi', ensureApi);
 
 dtcVersion = new ReactiveVar();
 Template.registerHelper('dtcVersion', function() {
