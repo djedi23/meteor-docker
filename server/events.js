9d4c43cf679d24c04d7a6ead4026ad30865317e2
Moïse Valvassori <moise.valvassori@gmail.com>
2015-04-21 06:53:10 +0200

    call hook on event

--- server/events.js
+++ server/events.js
@@ -2,31 +2,32 @@
 eventHandle = function(host, event){
   // event export
   switch(event.status){
-    case 'destroy':
+  case 'destroy':
     Containers.remove({_host: host, Id:event.id});
     ContainersInspect.remove({_host: host,Id:event.id});
     break;
-    case 'delete':
+  case 'delete':
     if (event.id){
       Images.remove({_host: host, Id:event.id});
       ImagesInspect.remove({_host: host, Id:event.id});
     }
     break;
-    case 'create':
-    case 'die':
-    case 'pause':
-    case 'kill':
-    case 'restart':
-    case 'start':
-    case 'stop':
-    case 'unpause':
+  case 'create':
+  case 'die':
+  case 'pause':
+  case 'kill':
+  case 'restart':
+  case 'start':
+  case 'stop':
+  case 'unpause':
     containerDetails(host, event.id);
     listContainers();
     startMonitoringContainer(host, event.id);
     break;
-    case 'untag':
+  case 'untag':
     listImages();
     imageDetail(host, event.id);
     break;
   }
+  modules.calls('events.'+event.status+'.'+event.id, this, host, event);
 };
