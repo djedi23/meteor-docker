020a67d659cf754a6b537f8e307f8334bf9cb80a
Moïse Valvassori <moise.valvassori@gmail.com>
2015-11-11 05:58:36 +0100

    handle more events

--- server/events.js
+++ server/events.js
@@ -1,5 +1,9 @@
 eventHandle = function(host, event){
   // event attach, commit, copy, exec_create, exec_start, export, oom, rename, resize, top
+/*
+Docker containers report the following events:
+attach, copy, exec_create, exec_start, export, oom, resize, top
+*/
   switch(event.status){
   case 'destroy':
     Containers.remove({_host: host, Id:event.id});
@@ -19,15 +23,21 @@ eventHandle = function(host, event){
   case 'start':
   case 'stop':
   case 'unpause':
+  case 'rename':
     containerDetails(host, event.id);
     listContainers();
     startMonitoringContainer(host, event.id);
     break;
+  case 'commit':
+  case 'import':
+  case 'pull':
+  case 'push':
   case 'untag':
+  case 'tag':
     listImages();
     imageDetail(host, event.id);
     break;
-    default:
+  default:
     console.log("unhandled EVENT",event);
   }
   modules.calls('events.'+event.status+'.'+event.id, this, host, event);
