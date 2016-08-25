c3cde55436ec9d10e52ddcbc5089d87a9568cb6e
moise <moise.valvassori@gmail.com>
2016-08-25 15:46:26 +0200

    ensure Api 1.24 for swarms operation

--- server/methods/swarms.js
+++ server/methods/swarms.js
@@ -3,15 +3,16 @@ listSwarms = function() {
     var managers = filterSwarmManager(docker);
     SwarmsInspect.remove({});
     _.each(managers, function(h){
-      Meteor.call('swarms.inspect',
-        {host:h[0]},
-        function(err,res){});
+      if (ensureApi(h[0], "1.24")) {
+        Meteor.call('swarms.inspect',
+          {host:h[0]},
+          function(err,res){});
+      }
     });
-
-//     SwarmsInspect.find().map(function(swarm){
-//       if (! _.detect(managers, function(m){ console.log(m[0] , swarm._id );  return m[0]===swarm._id; }))
-//         SwarmsInspect.remove({_id:swarm._id});
-//     });
+    //     SwarmsInspect.find().map(function(swarm){
+    //       if (! _.detect(managers, function(m){ console.log(m[0] , swarm._id );  return m[0]===swarm._id; }))
+    //         SwarmsInspect.remove({_id:swarm._id});
+    //     });
   });
 };
 
@@ -19,113 +20,117 @@ listSwarms = function() {
 Meteor.methods({
   'swarms.init': function(opts) {
     check(opts, swarmsInitSchemas);
-    if (!Roles.userIsInRole(Meteor.user(), ['admin', 'swarms.init']))
-      throw new Meteor.Error(403, "Not authorized to init swarms");
-    if (docker == undefined)
-      return null;
+    if (ensureApi(opts.host, "1.24")) {
+      if (!Roles.userIsInRole(Meteor.user(), ['admin', 'swarms.init']))
+        throw new Meteor.Error(403, "Not authorized to init swarms");
+      if (docker == undefined)
+        return null;
 
-    Future = Npm.require('fibers/future');
-    var myFuture = new Future();
-    var swarm = docker[opts.host].swarmInit(opts,function(err, result) {
-                  if (err) {
-                    console.log(err);
-                    myFuture.throw(err);
-                  }
-                  else {
-                    myFuture.return(result);
-                  }
-                });
-    try {
-      listSwarms();
-      return myFuture.wait();
+      Future = Npm.require('fibers/future');
+      var myFuture = new Future();
+      var swarm = docker[opts.host].swarmInit(opts,function(err, result) {
+                    if (err) {
+                      console.log(err);
+                      myFuture.throw(err);
+                    }
+                    else {
+                      myFuture.return(result);
+                    }
+                  });
+      try {
+        listSwarms();
+        return myFuture.wait();
+      }
+      catch (err) {
+        throw new Meteor.Error('docker', err.toString());
+      }
     }
-    catch (err) {
-      throw new Meteor.Error('docker', err.toString());
-    }
-
   },
   'swarms.join': function(opts) {
     check(opts, swarmsJoinSchemas);
-    if (!Roles.userIsInRole(Meteor.user(), ['admin', 'swarms.join']))
-      throw new Meteor.Error(403, "Not authorized to join swarms");
-    if (docker == undefined)
-      return null;
+    if (ensureApi(opts.host, "1.24")) {
+      if (!Roles.userIsInRole(Meteor.user(), ['admin', 'swarms.join']))
+        throw new Meteor.Error(403, "Not authorized to join swarms");
+      if (docker == undefined)
+        return null;
 
-    Future = Npm.require('fibers/future');
-    var myFuture = new Future();
-    var swarm = docker[opts.host].swarmJoin(opts,function(err, result) {
-                  if (err) {
-                    console.log(err);
-                    myFuture.throw(err);
-                  }
-                  else {
-                    myFuture.return(result);
-                  }
-                });
-    try {
-      listSwarms();
-      return myFuture.wait();
+      Future = Npm.require('fibers/future');
+      var myFuture = new Future();
+      var swarm = docker[opts.host].swarmJoin(opts,function(err, result) {
+                    if (err) {
+                      console.log(err);
+                      myFuture.throw(err);
+                    }
+                    else {
+                      myFuture.return(result);
+                    }
+                  });
+      try {
+        listSwarms();
+        return myFuture.wait();
+      }
+      catch (err) {
+        throw new Meteor.Error('docker', err.toString());
+      }
     }
-    catch (err) {
-      throw new Meteor.Error('docker', err.toString());
-    }
-
   },
   'swarms.update': function(opts) {
     check(opts, swarmsUpdateSchemas);
-    if (!Roles.userIsInRole(Meteor.user(), ['admin', 'swarms.update']))
-      throw new Meteor.Error(403, "Not authorized to update swarms");
-    if (docker == undefined)
-      return null;
+    if (ensureApi(opts.host, "1.24")) {
+      if (!Roles.userIsInRole(Meteor.user(), ['admin', 'swarms.update']))
+        throw new Meteor.Error(403, "Not authorized to update swarms");
+      if (docker == undefined)
+        return null;
 
-    Future = Npm.require('fibers/future');
-    var myFuture = new Future();
-    var swarm = docker[opts.host].swarmUpdate(opts,function(err, result) {
-                  if (err) {
-                    console.log(err);
-                    myFuture.throw(err);
-                  }
-                  else {
-                    myFuture.return(result);
-                  }
-                });
-    try {
-      listSwarms();
-      return myFuture.wait();
+      Future = Npm.require('fibers/future');
+      var myFuture = new Future();
+      var swarm = docker[opts.host].swarmUpdate(opts,function(err, result) {
+                    if (err) {
+                      console.log(err);
+                      myFuture.throw(err);
+                    }
+                    else {
+                      myFuture.return(result);
+                    }
+                  });
+      try {
+        listSwarms();
+        return myFuture.wait();
+      }
+      catch (err) {
+        throw new Meteor.Error('docker', err.toString());
+      }
     }
-    catch (err) {
-      throw new Meteor.Error('docker', err.toString());
-    }
-
   },
   'swarms.leave': function(opts) {
     check(opts, {
       host: checkHostId
     });
-    if (!Roles.userIsInRole(Meteor.user(), ['admin', 'swarms.leave']))
-      throw new Meteor.Error(403, "Not authorized to leave swarms");
-    if (docker == undefined)
-      return null;
+    if (ensureApi(opts.host, "1.24")) {
+      if (!Roles.userIsInRole(Meteor.user(), ['admin', 'swarms.leave']))
+        throw new Meteor.Error(403, "Not authorized to leave swarms");
+      if (docker == undefined)
+        return null;
 
-    Future = Npm.require('fibers/future');
-    var myFuture = new Future();
-    var swarm = docker[opts.host].swarmLeave(function(err, result) {
-                  if (err) {
-                    console.log(err);
-                    myFuture.throw(err);
-                  }
-                  else {
-                    myFuture.return(result);
-                  }
-                });
-    try {
-      listSwarms();
-      return myFuture.wait();
+      Future = Npm.require('fibers/future');
+      var myFuture = new Future();
+      var swarm = docker[opts.host].swarmLeave(function(err, result) {
+                    if (err) {
+                      console.log(err);
+                      myFuture.throw(err);
+                    }
+                    else {
+                      myFuture.return(result);
+                    }
+                  });
+      try {
+        listSwarms();
+        return myFuture.wait();
+      }
+      catch (err) {
+        throw new Meteor.Error('docker', err.toString());
+      }
     }
-    catch (err) {
-      throw new Meteor.Error('docker', err.toString());
-    }
-
   },
   'swarms.list': function(opts) {
     //    if (ensureApi(opts.host, "1.24")) {
@@ -180,6 +185,9 @@ Meteor.methods({
 
 const LISTS_METHODS = _.pluck([
   'swarms.list',
+  'swarms.leave',
+  'swarms.join',
+  'swarms.update',
   'swarms.inspect'
 ], 'name');
 DDPRateLimiter.addRule({
