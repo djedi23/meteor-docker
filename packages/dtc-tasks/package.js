250796bec5f293b78d89e542cf36c1449f0bddb1
moise <moise.valvassori@gmail.com>
2016-08-28 14:43:47 +0200

    add ecmascript to packages

--- packages/dtc-tasks/package.js
+++ packages/dtc-tasks/package.js
@@ -12,6 +12,7 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('1.2.1');
+  api.use('ecmascript', 'client');
   api.use([
     'alanning:roles',
     'templating',
@@ -21,7 +22,6 @@ Package.onUse(function(api) {
     'underscore',
     'dtc-json2table'
   ], 'client');
-
 
   api.use([
     'djedi:modules',
